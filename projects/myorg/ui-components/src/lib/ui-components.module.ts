14aaa0f22f30961ca3fd52dc47cb1f4b2b37c515
Ricardo Corbera <rdcorbera@gmail.com>
2022-06-29 19:55:28 -0500

    Create Button component

--- projects/myorg/ui-components/src/lib/ui-components.module.ts
+++ projects/myorg/ui-components/src/lib/ui-components.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { UiComponentsComponent } from './ui-components.component';
+import { ButtonComponent } from './button/button.component';
 
 
 
 @NgModule({
   declarations: [
-    UiComponentsComponent
+    ButtonComponent
   ],
   imports: [
   ],
   exports: [
-    UiComponentsComponent
+    ButtonComponent
   ]
 })
 export class UiComponentsModule { }
